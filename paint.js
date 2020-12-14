f385f8a3cd6bbb541b06c17350ae6d7b6bcca7a3
Balaaji Muthukumar <mbalaji1995@gmail.com>
2020-12-15 02:22:26 +0530

    feat - adding a new type of brush

--- paint.js
+++ paint.js
@@ -1,7 +1,8 @@
 var myCanvas = document.getElementById("myCanvas");
 var ctx = myCanvas.getContext("2d");
-var x = 0;
-var y = 0;
+var x = [];
+var y = [];
+var state = "";
 //updating the values in real time
 myCanvas.addEventListener('mousemove',event=>valueUpdate(event));
 
@@ -9,23 +10,33 @@ myCanvas.addEventListener('mousemove',event=>valueUpdate(event));
 myCanvas.addEventListener('mousedown',e =>{
     ctx.beginPath();
     ctx.moveTo(x, y);
+    state = "On";
+    console.log(state);
     //painting across the travelled places
     myCanvas.addEventListener('mousemove',lineSegment);
 });
 
 myCanvas.addEventListener('mouseup',e =>{
-    console.log("donee");
+    console.log("done");
+    state = "";
+    console.log(state);
     lineSegment();
     myCanvas.removeEventListener('mousemove',lineSegment);
 });
+
 function valueUpdate(event){
-    x = event.offsetX;
-    y = event.offsetY;
+    x.push(event.offsetX);
+    y.push(event.offsetY);
     document.getElementById("mydata").innerText = " "+x+" "+y;
 }
+
 function lineSegment(){
-    ctx.lineTo(x, y); 
-    ctx.stroke();
+if(state==="on"){
+    for(let i = 0;i<=x.length;i++){
+        ctx.lineTo(x, y); 
+        ctx.stroke();
+    }
+}
 }
 
 
