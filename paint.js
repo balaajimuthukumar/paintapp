1f81d09d14b99430b689433664ca155cf50f74c4
Balaaji Muthukumar <mbalaji1995@gmail.com>
2020-12-15 22:57:08 +0530

    refactor - beta version

--- paint.js
+++ paint.js
@@ -1,8 +1,20 @@
 var myCanvas = document.getElementById("myCanvas");
 var ctx = myCanvas.getContext("2d");
-var x = [];
-var y = [];
-var state = "";
+var query = document.getElementsByTagName("button");
+var x = 0;
+var y = 0;
+var spreadx = 0;
+var spready = 0;
+var func = eval("spray");
+var color ='#000000';
+
+//listens to all the buttons and select the 
+for (i = 0; i < query.length; i++) {
+    query[i].addEventListener("click", function(event) {
+        func = window[event.target.id]; 
+    });
+    }
+
 //updating the values in real time
 myCanvas.addEventListener('mousemove',event=>valueUpdate(event));
 
@@ -10,35 +22,68 @@ myCanvas.addEventListener('mousemove',event=>valueUpdate(event));
 myCanvas.addEventListener('mousedown',e =>{
     ctx.beginPath();
     ctx.moveTo(x, y);
-    state = "On";
-    console.log(state);
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
     //painting across the travelled places
-    myCanvas.addEventListener('mousemove',lineSegment);
+    myCanvas.addEventListener('mousemove',func);
 });
 
+//Stops drawing when the mouse click is released
 myCanvas.addEventListener('mouseup',e =>{
-    console.log("done");
-    state = "";
-    console.log(state);
-    lineSegment();
-    myCanvas.removeEventListener('mousemove',lineSegment);
+    myCanvas.removeEventListener('mousemove',func);
 });
 
+//
 function valueUpdate(event){
-    x.push(event.offsetX);
-    y.push(event.offsetY);
-    document.getElementById("mydata").innerText = " "+x+" "+y;
+    x = event.offsetX;  y = event.offsetY;   
+    document.getElementById("mydata").innerText = "X :"+x+" ; "+"Y :"+y;
+}
+
+function brush(){
+    ctx.lineJoin = ctx.lineCap = 'butt'; 
+    ctx.shadowBlur = 0;
+    ctx.shadowColor = 'rgb(0, 0, 0)';  
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    ctx.lineTo(x+5,y+5);
+    ctx.stroke();
 }
 
 function lineSegment(){
-if(state==="on"){
-    for(let i = 0;i<=x.length;i++){
-        ctx.lineTo(x, y); 
-        ctx.stroke();
-    }
-}
+    ctx.shadowBlur = 0;
+    ctx.lineJoin = ctx.lineCap = 'round'; 
+    ctx.lineTo(x,y);
+    ctx.stroke();
 }
 
+function spray(){
+    spreadx = Math.random()*20;
+    spready = Math.random()*20; 
+    ctx.lineJoin = ctx.lineCap = 'round'; 
+    ctx.fillRect(x+spreadx,y-spready,2,2);
+}
 
+function smudge(){
+    ctx.lineJoin = ctx.lineCap = 'round'; 
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = 'rgb(0, 0, 0)';
+    ctx.lineTo(x,y);
+    ctx.stroke();     
+}
 
+function pallete(id){
+    color="#"+id;
+}
 
+function eraser(){
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = 'rgb(255, 255, 255)';
+    ctx.lineJoin = ctx.lineCap = 'round'; 
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.lineTo(x,y);
+    ctx.stroke();
+}
+
+function size(string){
+    ctx.lineWidth = string;
+}
